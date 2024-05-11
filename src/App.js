8df3a4a65a49b0e008819e39f5ecb0489984deb9
CHEDDARcb <114633645+CHEDDARcb@users.noreply.github.com>
2024-05-11 15:57:15 +0900

    add Packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
-/*
 import { Banner } from './components/Banner';
+/*
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Footer } from './components/Footer';*/
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      {/*<Banner />
-      <Skills />
+      <Banner />
+      {/*<Skills />
       <Projects />
       <Footer />*/}
     </div>
